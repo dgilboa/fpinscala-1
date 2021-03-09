90c05ada0504c6e7bae81a0c3eca51d3c235e256
Dror Gilboa <dror@lightricks.com>
2021-03-09 22:34:12 +0200

    DataStructures: Add solution to most of List.

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -50,17 +50,43 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
 
-  def tail[A](l: List[A]): List[A] = ???
+  def tail[A](l: List[A]): List[A] = l match {
+      case Nil => Nil
+      case Cons(_, t) => t
+    }
 
-  def setHead[A](l: List[A], h: A): List[A] = ???
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+      case Nil => Cons(h, Nil)
+      case Cons(_, t) => Cons(h, t)
+    }
 
-  def drop[A](l: List[A], n: Int): List[A] = ???
+  def drop[A](l: List[A], n: Int): List[A] = {
+    if (n <= 0) l
+    else
+      l match {
+        case Nil  => Nil
+        case Cons(_, t)  => drop(t, n - 1)
+      }
+  }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Cons(h, t) if (f(h)) => dropWhile(t, f)
+    case _ => l // we actually don't care if it's Nil or cons(h,t)
+  }
 
   def init[A](l: List[A]): List[A] = ???
 
-  def length[A](l: List[A]): Int = ???
+  /** with out fold right */
+  def length[A](l: List[A]): Int = {
+    def len(l:List[A], acc: Int): Int = l match {
+      case Cons(h, t) => len(t, acc+1)
+      case Nil => acc
+    }
+    len(l, 0)
+  }
+
+  def length[A](l: List[A]): Int =
+    foldRight(l,0)((_, acc) => acc + 1)
 
   def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
 
